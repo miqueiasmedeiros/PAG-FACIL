8ab3a95bd5a5185923a35a376e9060db613fee8a
miqueiasmedeiros <miqueiascompany@gmail.com>
2026-04-01 18:40:56 -0300

    feat: created transaction entity

---
+++ src/main/java/com/pagamento/pag_facil/domain/transaction/Transaction.java
@@ -0,0 +1,32 @@
+package com.pagamento.pag_facil.domain.transaction;
+
+import com.pagamento.pag_facil.domain.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity(name = "transactions")
+@Table(name = "transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+    private LocalDateTime timestamp;
+}
