62d097c245d1bfb29bbc45a73cd3c76f4b50df90
miqueiasmedeiros <miqueiascompany@gmail.com>
2026-04-01 22:24:33 -0300

    Created NotificationException and updated

---
+++ src/main/java/com/pagamento/pag_facil/exceptions/NotificationException.java
@@ -0,0 +1,7 @@
+package com.pagamento.pag_facil.exceptions;
+
+public class NotificationException extends RuntimeException {
+    public NotificationException(String message) {
+        super(message);
+    }
+}
